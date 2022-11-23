9a535dc7114bd2c55674b97eefda16d1de7cc491
Mark Johnson <markfjohnson@gmail.com>
2022-11-23 09:55:25 -0500

    API Usage Plan components added.  TODO Map the usage plan to the API

--- api_gw_examples/api_gw_examples_stack.py
+++ api_gw_examples/api_gw_examples_stack.py
@@ -35,41 +35,47 @@ class ApiGwExamplesStack(Stack):
         )
         return(lambda_func)
 
-    def defineApiGateway(self, apiName, description, myFunction=None, apikey_required=False):
-        # log_group = logs.LogGroup(self, f"{apiName}-{stageName}Logs")
-        # deploy_options = apigateway.StageOptions(stage_name=stageName, description="CDK Sample Prod Stage Option",
-        #                                          logging_level=apigateway.MethodLoggingLevel.INFO
-        #                                          )
-        request_params = []
-        method_option = apigateway.MethodOptions(api_key_required=apikey_required, request_parameters=request_params)
-        if myFunction is not None:
-            lambda_proxy = True
-        else:
-            lambda_proxy = False
+    def defineApiGateway(self, apiName, description, myFunction=None, apikey_required=False, integrationType=None, endpointType=None):
+
+        # request_params = []
+        # method_option = apigateway.MethodOptions(api_key_required=apikey_required, request_parameters=request_params)
+        # if myFunction is not None:
+        #     lambda_proxy = True
+        # else:
+        #     lambda_proxy = False
 
         api = apigateway.RestApi(
             self, apiName,
-            proxy=lambda_proxy,
+            rest_api_name=apiName,
             description=description,
-            # deploy_options=deploy_options,
-            deploy=True,
-            handler=myFunction,
-            default_method_options=method_option
+            default_integration=integrationType,
+            endpoint_configuration=endpointType,
         )
 
         return api
 
-    def defineUsagePlan(self, usagePlanName, api):
-        plan = api.add_usage_plan(usagePlanName, name=usagePlanName,
+    def defineMethods(self, api, integrationType):
+        v1 = api.root.add_resource("v1")
+        echo = v1.add_resource("echo")
+        echo_method = echo.add_method("GET", integrationType, api_key_required=True)
+
+    def defineUsagePlan(self, api, usagePlanName, integrationType=None):
+
+        plan = api.add_usage_plan(usagePlanName,
+                                  name=usagePlanName,
                                   throttle=apigateway.ThrottleSettings(
                                       rate_limit=10,
                                       burst_limit=2
                                   )
                                   )
+
+        key = api.add_api_key("ApiKey")
+        plan.add_api_key(key)
+
         return(plan)
 
     def addApiKey(self, apiKeyName, usagePlan, api):
-        key = api.add_api_key(apiKeyName)
+        key = api.add_api_key(apiKeyName, api_key_name=apiKeyName)
         usagePlan.add_api_key(key)
         return key
 
@@ -80,15 +86,32 @@ class ApiGwExamplesStack(Stack):
         #
         return None
 
-    def addStageToApi(self, apigw, stageName, deploymentName, cacheEnabled=False, tacingEnabled=True,
-                      retainDeployment=False, stageVariables=None):
-        #log_group = logs.LogGroup(self, f"{stageName}-StageLogs")
-        deployment = apigateway.Deployment(self, deploymentName, api=apigw, retain_deployments=retainDeployment)
-        new_stage = apigateway.Stage(self, stageName, description=f"{stageName} - an additional stage",
-                         deployment=deployment, stage_name=stageName, tracing_enabled=tacingEnabled,
-                                     caching_enabled=cacheEnabled, variables=stageVariables
-                         )
-        return new_stage
+    # def addStageToApi(self, apigw, stageName, deploymentName, cacheEnabled=False, tacingEnabled=True,
+    #                   retainDeployment=False, stageVariables=None):
+    #     #log_group = logs.LogGroup(self, f"{stageName}-StageLogs")
+    #     deployment = apigateway.Deployment(self, deploymentName, api=apigw, retain_deployments=retainDeployment)
+    #     new_stage = apigateway.Stage(self, stageName, description=f"{stageName} - an additional stage",
+    #                      deployment=deployment, stage_name=stageName, tracing_enabled=tacingEnabled,
+    #                                  caching_enabled=cacheEnabled, variables=stageVariables
+    #                      )
+    #     return new_stage
+
+
+    #
+    ##
+    ###
+    #   Example functions
+
+    def ex_regional_usageplan(self):
+        api_gw = self.defineApiGateway("UsagePlan", "This is a test API Gateway with Lambda Integration")
+        self.defineMethods(api_gw, integrationType=None)
+        #stage = self.addStageToApi(api_gw, "Prod", "deployment2", cacheEnabled=True, retainDeployment=True)
+        plan = self.defineUsagePlan(api_gw, "TestUsagePlan")
+        new_key = self.addApiKey("Customer1", plan, api_gw)
+        new_key = self.addApiKey("Customer2", plan, api_gw)
+        new_key = self.addApiKey("Customer3", plan, api_gw)
+        return api_gw
+
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         func = self.defineFunction("APIGwTestFunction")
@@ -98,17 +121,8 @@ class ApiGwExamplesStack(Stack):
         public_stage_variables = {
             "year": "2022"
         }
-        api_gw = self.defineApiGateway("UsagePlan", "This is a test API Gateway with Lambda Integration", func)
-        stage = self.addStageToApi(api_gw, "Produ", "deployment2", cacheEnabled=True, retainDeployment=True)
-        plan = self.defineUsagePlan("TestUsagePlan", api_gw)
-        new_key = self.addApiKey("Customer1", plan, api_gw)
-        new_key = self.addApiKey("Customer2", plan, api_gw)
-        new_key = self.addApiKey("Customer3", plan, api_gw)
+        api_gw = self.ex_regional_usageplan()
 
 
-        public_stage_variables = {
 
-        }
-        pri_api_gw = self.defineApiGateway("PublicParams", "This is a test API Gateway with Lambda Integration", func)
-        stage = self.addStageToApi(api_gw, "Prod", "deployment1", cacheEnabled=True, retainDeployment=True, stageVariables=public_stage_variables)
 
